23d3ab41a18e42bf924d0014548b6fac1b023108
miciella <201910804@feualabang.edu.ph>
2022-06-03 18:32:30 +0800

    sana pwede to plz

--- main.js
+++ main.js
@@ -50,40 +50,40 @@
 //
 // console.log(data)
 //
-const input = "(";
-
-const KEYWORDS = {
-    if : 'keyword',
-    el : 'keyword',
-    elf : 'keyword',
-    show : 'keyword',
-    shown : 'keyword',
-    inp : 'keyword',
-    '(' : 'open-parenthesis',
-    ')' : 'close-parenthesis',
-}
-
-//trial - isa lang gumagana na lexeme
-function lexer(str) {
-    let arr = str.split(' ');
-    let size = Object.keys(KEYWORDS).length
-
-    for (let i = 0; i < size; i++) {
-        let propertyNames = Object.keys(KEYWORDS)[i];
-        let propertyValues = Object.values(KEYWORDS)[i];
-        for (let index = 0; index < arr.length; index++) {
-            if (arr[index] === propertyNames) {
-                return {
-                    lexeme: propertyNames,
-                    type: propertyValues,
-                };
-            }
-        }
-    }
-}
-
-
-console.log(lexer(input))
+// const input = "(";
+//
+// const KEYWORDS = {
+//     if : 'keyword',
+//     el : 'keyword',
+//     elf : 'keyword',
+//     show : 'keyword',
+//     shown : 'keyword',
+//     inp : 'keyword',
+//     '(' : 'open-parenthesis',
+//     ')' : 'close-parenthesis',
+// }
+//
+// //trial - isa lang gumagana na lexeme
+// function lexer(str) {
+//     let arr = str.split(' ');
+//     let size = Object.keys(KEYWORDS).length
+//
+//     for (let i = 0; i < size; i++) {
+//         let propertyNames = Object.keys(KEYWORDS)[i];
+//         let propertyValues = Object.values(KEYWORDS)[i];
+//         for (let index = 0; index < arr.length; index++) {
+//             if (arr[index] === propertyNames) {
+//                 return {
+//                     lexeme: propertyNames,
+//                     type: propertyValues,
+//                 };
+//             }
+//         }
+//     }
+// }
+//
+//
+// console.log(lexer(input))
 
 // const input = "if elf else"
 // const lexer = (str) =>
@@ -94,3 +94,47 @@ console.log(lexer(input))
 //
 // console.log(lexer(input)) // outputs ["John" , "Doe"]
 
+'use strict';
+
+const Lex = require('lexical-parser')
+
+let input = 'block { int a = 0 int b = 3 if (a < b && b > a) { print("Hello World") } }'
+
+// You can specify an exact string or a regex for the token
+let tokenMatchers = [
+    'block',
+    'if',
+    '{',
+    '}',
+    '(',
+    ')',
+    '<',
+    '>',
+    '=',
+    '&&',
+    'int',
+    ['integer', /[0-9]+/],
+    ['id', /[a-zA-Z][a-zA-Z0-9]*/],
+    ['string', /\".*?\"/]
+]
+// The pattern to ignore in the input
+let ignorePattern = '[\n\s \t]+'
+
+let lex = new Lex(input, tokenMatchers, ignorePattern)
+let token = undefined
+try {
+    do {
+        token = lex.nextToken()
+        console.log(token)
+    } while (token)
+} catch (err) {
+    // Error handling
+    if (err.code === "LEXICAL_ERROR") {
+        console.log(`\n${err.message}\n`)
+        console.log(`Position: ${err.position}`)
+        console.log(`Character: ${err.character}`)
+        console.log(`Nearby code: ${err.nearbyCode}`)
+    }
+    else
+        console.log(err)
+}
