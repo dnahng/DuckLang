13ad745902032b80f0c10d7fc8f73642494cb0ba
erstinn <erstin.adean@gmail.com>
2022-07-02 14:39:24 +0800

    fixed user input

--- trial_lexer/js.js
+++ trial_lexer/js.js
@@ -1,10 +1,20 @@
 import { readFileSync } from 'fs'
 import { lexer } from "./lexer.js"
+import * as readline from 'node:readline/promises';
+import { stdin as input, stdout as output } from 'node:process';
+
 
 const file = "./source.txt";
 
-const input = String(readFileSync(file));
-const prompt = require('prompt-sync')();
+//changed var-name for file's input
+const file_input = String(readFileSync(file));
+
+//old:
+// const input = String(readFileSync(file));
+// const prompt = require('prompt-sync')();
+
+//input var.name is instead used in stdin
+const rl = readline.createInterface({ input, output });
 let ans;
 do{
     console.log("MAIN MENU: ");
@@ -14,36 +24,43 @@ do{
     console.log("4: View Syntax Error");
     console.log("5: View Semantic Error");
     console.log("6: Exit Program");
-    ans = prompt("Please input your choice: ");
+    // let ans = prompt("Please input your choice: ")  ;
+    ans = await rl.question('Please input your choice');
 
+    //changed ans from int to string since JS gaming :)
     switch(ans){
-        case 1:
+        case '1':
+            console.log("1");
+
             break;
-        case 2:
+
+        case '2':
+            console.log("2");
             console.log("start");
 
-            for(const token of lexer(file, input)){
+            for(const token of lexer(file_input, input)){
                 console.log(token);
             }
 
             console.log('finish');
             break;
-        case 3:
+        case '3':
             break;
-        case 4:
+        case '4':
             break;
-        case 5:
+        case '5':
             break;
-        case 6:
+        case '6':
             console.log("You have exited the program");
-
             break;
         default:
             console.log("Invalid input, please try again");
             break;
     }
     console.log("Do you want")
-}while (ans !== 6);
+}while (ans !== '6');
+rl.close();
+
 
 // read file
 // console.log("start");
