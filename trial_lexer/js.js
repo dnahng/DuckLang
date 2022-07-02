10fc5545fe2c0436bf2d93ef82c215bbf1f6796e
dnahng <201911863@feualabang.edu.ph>
2022-07-02 20:54:47 +0800

    edited menu a bit

--- trial_lexer/js.js
+++ trial_lexer/js.js
@@ -2,21 +2,73 @@ import { readFileSync } from 'fs'
 import { lexer } from "./lexer.js"
 import * as readline from 'node:readline/promises';
 import { stdin as input, stdout as output } from 'node:process';
+import pressAnyKey from 'press-any-key';
+
+//functions
+function delay(time) {
+    return new Promise(resolve => setTimeout(resolve, time));
+}
+
+// KEY PRESS FUNCS, DITO MUNA WILL RESOLVENNALANG LATER
+// const keypress = async () => {
+//     process.stdin.setRawMode(true)
+//     return new Promise(resolve => process.stdin.once('data', () => {
+//         process.stdin.setRawMode(false)
+//         resolve()
+//     }))
+// }
 
 
+// function keyPress(message, keys) {
+//     const _message = message || "Press any key to continue...";
+//     const _keys = keys || "";
+//     return new Promise(function (resolve, reject) {
+//         const caseSensitive = _keys.toLowerCase() !== _keys && _keys.toUpperCase() !== _keys;
+//         process.stdout.write(_message);
+//         function keyListener(buffer) {
+//             let key = buffer.toString();
+//             if (key.charCodeAt(0) === 3) {
+//                 process.stdin.setRawMode(false);
+//                 process.stdin.off('data', keyListener);
+//                 process.stdin.pause();
+//                 // process.exit(0); // Exit process if you prefer.
+//                 reject(key.charCodeAt(0));
+//             }
+//             const index = caseSensitive ? _keys.indexOf(key) : _keys.toLowerCase().indexOf(key.toLowerCase());
+//             if (_keys && index < 0) {
+//                 process.stdout.write(key);
+//                 process.stdout.write("\n");
+//                 process.stdout.write(_message);
+//                 return;
+//             }
+//             process.stdin.setRawMode(false);
+//             process.stdin.off('data', keyListener);
+//             process.stdin.pause();
+//             if (index >= 0) {
+//                 key = _keys.charAt(index);
+//                 process.stdout.write(key);
+//             }
+//             process.stdout.write("\n");
+//             resolve(key);
+//         }
+//         process.stdin.resume();
+//         process.stdin.setRawMode(true);
+//         process.stdin.on('data', keyListener);
+//     });
+// }
+
+//var declaration
 const file = "./source.txt";
-
-//changed var-name for file's input
 const file_input = String(readFileSync(file));
-
-//old:
-// const input = String(readFileSync(file));
-// const prompt = require('prompt-sync')();
-
-//input var.name is instead used in stdin
 const rl = readline.createInterface({ input, output });
+
+
+// const pressAnyKey = require('press-any-key');
 let ans;
+
+//menu
 do{
+    console.clear();
     console.log("MAIN MENU: ");
     console.log("1: Upload Text File");
     console.log("2: View Lexemes/Token");
@@ -25,7 +77,7 @@ do{
     console.log("5: View Semantic Error");
     console.log("6: Exit Program");
     // let ans = prompt("Please input your choice: ")  ;
-    ans = await rl.question('Please input your choice');
+    ans = await rl.question('Please input your choice: ');
 
     //changed ans from int to string since JS gaming :)
     switch(ans){
@@ -35,12 +87,29 @@ do{
             break;
 
         case '2':
+            //TEST CODE
+        //     ;(async () => {
+        //
+        //     console.log('program started, press any key to continue')
+        //     await keypress()
+        //     console.log('program still running, press any key to continue')
+        //     await keypress()
+        //     console.log('bye')
+        //     process.stdin.pause();
+        //
+        // })().then(process.stdin.pause)
+
             console.log("2");
             console.log("start");
-            for(const token of lexer(file, file_input)){
+            for (const token of lexer(file, file_input)) {
                 console.log(token);
             }
             console.log('finish');
+            // console.log("press any key to go back to menu")
+
+
+            await delay(10000);
+            console.log("you have returned");
             break;
         case '3':
             break;
@@ -55,7 +124,6 @@ do{
             console.log("Invalid input, please try again");
             break;
     }
-    console.log("Do you want")
 }while (ans !== '6');
 rl.close();
 
